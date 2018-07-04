d069fae2ed49471a10e8c6b8f62123d08c91dd41
Emanuel <eurena@soin.co.cr>
2018-07-03 18:28:49 -0600

    Fuentes

---
+++ src/co/cr/DocManager/db/entities/Application.java
@@ -0,0 +1,14 @@
+package cr.co.DocManager.db.entities;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SOIN
+ */
+public class Application {
+    
+}
